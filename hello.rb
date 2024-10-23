79b50623153b8633666b04e1176a706dbe1d82a7
karlaechartea <k.echartea@chicagobooth.edu>
2024-10-23 16:38:27 +0000

    commit

--- hello.rb
+++ hello.rb
@@ -1,11 +1,14 @@
+require "active_support/all"
+
 pp "hello, world!"
 
 require "./bye.rb"
-
-require "active_support/all"
 
 pp "What's your name?"
 name = gets.chomp #add chomp to avoid the /n character that appears by default
 
 message = "Good morning, " + name + "!"
 pp message
+
+pp 4.to_s + "th"
+pp 1.ordinalize
